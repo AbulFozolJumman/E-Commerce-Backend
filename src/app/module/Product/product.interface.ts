5824076743fbff3bbb33501ee1069af375b334be
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-07-04 00:36:08 +0600

    Prduct type writting done

---
+++ src/app/module/Product/product.interface.ts
@@ -0,0 +1,20 @@
+// types.ts
+export type IVariant = {
+  type: string;
+  value: string;
+};
+
+export type IInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type IProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: IVariant[];
+  inventory: IInventory;
+};
